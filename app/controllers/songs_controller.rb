6f6f148b597ac8ddaf71b6714c39faeed1c6cb4f
dieptran <dieptx.ptit@gmail.com>
2017-01-16 21:01:31 +0700

    Create comment song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,14 @@
 class SongsController < ApplicationController
   load_resource only: :show
+  before_action :song_support, only: :show
 
   def show
+    @commentable = @song
+    @comment = current_user.comments.new
+  end
+
+  private
+  def song_support
+    @support = Supports::SongSupport.new @song
   end
 end
