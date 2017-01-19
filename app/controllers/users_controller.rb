caff664866a5e63257ee31db0b3d8aae949cd81f
ducbka <trungducphan95@gmail.com>
2017-01-19 23:23:13 +0700

    friendly-url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  load_resource
+  load_and_authorize_resource find_by: :slug
+
+  def index
+    @search = User.search params[:q]
+    @users = @search.result.all_customer.paginate page: params[:page],
+      per_page: Settings.users.order_per_page
+  end
 
   def show
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117174135) do
+ActiveRecord::Schema.define(version: 20170119160522) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -113,6 +113,18 @@ ActiveRecord::Schema.define(version: 20170117174135) do
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  end
+
   create_table "impressions", force: :cascade do |t|
     t.string   "impressionable_type"
     t.integer  "impressionable_id"
@@ -203,6 +215,8 @@ ActiveRecord::Schema.define(version: 20170117174135) do
     t.integer  "singer_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "slug"
+    t.index ["slug"], name: "index_songs_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -223,9 +237,11 @@ ActiveRecord::Schema.define(version: 20170117174135) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "slug"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
 end
