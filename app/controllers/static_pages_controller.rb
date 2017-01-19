9a46e7c96360f9d51a3d675cc832baadd46dd3bf
dieptran <dieptx.ptit@gmail.com>
2017-01-19 11:18:35 +0700

    UI Ux

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def show
     if valid_page?
+      if params[:page] == "home"
+        @songs = Song.all
+        @albums = Album.all
+      end
       render template: "static_pages/#{params[:page]}"
     else
       render file: "public/404.html", status: :not_found
