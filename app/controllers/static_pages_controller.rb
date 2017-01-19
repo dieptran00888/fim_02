41f1ed1ff22228263553ef4bc11ded2d7fe6ca7d
dieptran <dieptx.ptit@gmail.com>
2017-01-19 23:08:05 +0700

    Fix nav login

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
   def show
-    if valid_page?
-      if params[:page] == "home"
-        @songs = Song.all
-        @albums = Album.all
+      if valid_page?
+        if params[:page] == "home"
+          @songs = Song.all
+          @albums = Album.all
+        end
+        render template: "static_pages/#{params[:page]}"
+      else
+        render file: "public/404.html", status: :not_found
       end
-      render template: "static_pages/#{params[:page]}"
-    else
-      render file: "public/404.html", status: :not_found
-    end
   end
 
   private
