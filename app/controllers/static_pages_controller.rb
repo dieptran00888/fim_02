2d2115a473e63fd7a61fb7807e90dfb4a4abc4b5
dieptran <dieptx.ptit@gmail.com>
2017-02-02 01:39:07 +0700

    Fix after Lunner New Year 2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,15 @@
 class StaticPagesController < ApplicationController
   def show
-      if valid_page?
-        if params[:page] == "home"
-          @albums = Album.all
-          @search = Song.ransack params[:q]
-          @songs = @search.result.includes :albums, :artists
-        end
-        render template: "static_pages/#{params[:page]}"
-      else
-        render file: "public/404.html", status: :not_found
+    if valid_page?
+      if params[:page] == "home"
+        @albums = Album.all
+        @search = Song.ransack params[:q]
+        @songs = @search.result.includes :albums, :artists
       end
+      render template: "static_pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
   end
 
   private
