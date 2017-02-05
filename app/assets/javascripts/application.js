b0c1fceca742a15968528e2586da9e6a14c23775
dieptran <dieptx.ptit@gmail.com>
2017-02-05 16:09:07 +0700

    Clear js in application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,51 +23,51 @@
 //= require ckeditor/init
 //= require chosen-jquery
 
-$(document).on('turbolinks:load', function () {
-
-  if($('#audio-player').length) {
-    $current = 1;
-    $('.list-group-item').eq($current-1).css('background', 'wheat');
-    $('.contribute-lyric').hide();
-    $('.contribute-lyric').eq($current-1).show();
-    $('.tabs-lyric').hide();
-    $('.tabs-lyric').eq($current-1).show();
-    $('.tab-lyric-content').hide();
-    $('.tab-lyric-content').eq($current-1).show();
-    $('#audio-player').on('ended', function () {
-      $source='';
-      $('.contribute-lyric').eq($current-1).hide();
-      $('.tabs-lyric').eq($current-1).hide();
-      $('.tab-lyric-content').eq($current-1).hide();
-      $('.list-group-item').eq($current-1).css('background', 'none');
-      $('#audio-player').attr('src', nextSource());
-    });
-
-    $('.reply').hide();
-    $('.button-reply').on('click', function () {
-      $(this).parent().next().show();
-    });
-
-    $('.chosen-select').chosen();
-    $('.chosen-container-single').css('width', '250px');
-
-    $('.icon-next').on('click', function () {
-      $('.list-group-item').eq($current-1).css('background', 'none');
-      $('#audio-player').attr('src', nextSource());
-    });
-  } else {
-
-  }
-
-  CKEDITOR.replace('#ckeditor_content');
-});
-
-function nextSource() {
-  var src = $('.list-group').children().children().eq($current).attr('data');
-  $('.list-group-item').eq($current).css('background', 'wheat');
-  $('.contribute-lyric').eq($current).show();
-  $('.tabs-lyric').eq($current).show();
-  $('.tab-lyric-content').eq($current).show();
-  $current++;
-  return src;
-}
+// $(document).on('turbolinks:load', function () {
+//
+//   if($('#audio-player').length) {
+//     $current = 1;
+//     $('.list-group-item').eq($current-1).css('background', 'wheat');
+//     $('.contribute-lyric').hide();
+//     $('.contribute-lyric').eq($current-1).show();
+//     $('.tabs-lyric').hide();
+//     $('.tabs-lyric').eq($current-1).show();
+//     $('.tab-lyric-content').hide();
+//     $('.tab-lyric-content').eq($current-1).show();
+//     $('#audio-player').on('ended', function () {
+//       $source='';
+//       $('.contribute-lyric').eq($current-1).hide();
+//       $('.tabs-lyric').eq($current-1).hide();
+//       $('.tab-lyric-content').eq($current-1).hide();
+//       $('.list-group-item').eq($current-1).css('background', 'none');
+//       $('#audio-player').attr('src', nextSource());
+//     });
+//
+//     $('.reply').hide();
+//     $('.button-reply').on('click', function () {
+//       $(this).parent().next().show();
+//     });
+//
+//     $('.chosen-select').chosen();
+//     $('.chosen-container-single').css('width', '250px');
+//
+//     $('.icon-next').on('click', function () {
+//       $('.list-group-item').eq($current-1).css('background', 'none');
+//       $('#audio-player').attr('src', nextSource());
+//     });
+//   } else {
+//
+//   }
+//
+//   CKEDITOR.replace('#ckeditor_content');
+// });
+//
+// function nextSource() {
+//   var src = $('.list-group').children().children().eq($current).attr('data');
+//   $('.list-group-item').eq($current).css('background', 'wheat');
+//   $('.contribute-lyric').eq($current).show();
+//   $('.tabs-lyric').eq($current).show();
+//   $('.tab-lyric-content').eq($current).show();
+//   $current++;
+//   return src;
+// }
